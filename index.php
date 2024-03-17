0c09612639743f70249cc6f77d5fda5ab22aa5c4
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2024-03-17 14:19:35 +0700

    13/122

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 echo "Xin chào PHP <br> <h1> Chữ h1 <h1>";
 
 $day_la_bien = "Bảo";
-// echo "Xin chào tôi là $day_la_bien";
-echo $day_la_bien . "<br>" . $day_la_bien . " đây là ký tự";
+echo "Xin chào tôi là $day_la_bien";
+echo $day_la_bien . "<br> đây là biến:" . $day_la_bien . " nối với ký tự";
+
+//Đây là Hằng
+define("hang", "10");
+echo "<br>Đây là hằng: " . hang;
+
+//print
+print "<br>print giống echo vl";
+//
+$caulenhIf = 9;
+if ($caulenhIf > 1) {
+    echo "<br>$caulenhIf > 1 =))";
+}
 ?>
