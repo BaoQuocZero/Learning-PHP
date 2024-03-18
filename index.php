d8efe8cb50924d450209884fe2c2e98c06b37f1f
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2024-03-18 19:39:28 +0700

    Kiểm tra kiểu dữ liệu

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
-echo "Xin chào PHP <br> <h1> Chữ h1 <h1>";
 
-$day_la_bien = "Bảo";
-echo "Xin chào tôi là $day_la_bien";
-echo $day_la_bien . "<br> đây là biến:" . $day_la_bien . " nối với ký tự";
+$a = "thuong";
+var_dump($a);
 
-//Đây là Hằng
-define("hang", "10");
-echo "<br>Đây là hằng: " . hang;
-
-//print
-print "<br>print giống echo vl";
-//
-$caulenhIf = 9;
-if ($caulenhIf > 1) {
-    echo "<br>$caulenhIf > 1 =))";
-}
 ?>
