6876750e9b921ba0ad524cd4c7d3ac4e3d97ff1e
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2024-12-16 18:59:15 +0700

    boostrap

---
+++ webbansachOnTap/footer.php
@@ -0,0 +1,55 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+<td width="200" valign="top" bgcolor="#92D84E">
+    <?php if (isset($_SESSION['username'])): ?>
+    <!-- Hiển thị thông tin tài khoản khi đã đăng nhập -->
+    <table cellpadding="0" cellspacing="0">
+        <tr>
+            <th colspan="2">
+                <font color="#ff6600">Thông tin tài khoản</font>
+            </th>
+        </tr>
+        <tr>
+            <td colspan="2">Xin chào:
+                <strong><?php echo htmlspecialchars($_SESSION['role']);?>: </strong>
+                <strong><?php echo htmlspecialchars($_SESSION['username']); ?></strong>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2"><a href="views/ThongTinUser.php">Xem thông tin tài khoản</a></td>
+        </tr>
+        <tr>
+            <td colspan="2"><a href="views/DoiMatKhau.php">Đổi mật khẩu</a></td>
+        </tr>
+        <tr>
+            <td colspan="2"><a href="views/logout.php">Đăng xuất</a></td>
+        </tr>
+    </table>
+    <?php else: ?>
+    <!-- Hiển thị form đăng nhập nếu chưa đăng nhập -->
+    <form action="xly_dangnhap.php" name="frmDN" method="post">
+        <table cellpadding="0" cellspacing="0">
+            <tr>
+                <th colspan="2">
+                    <font color="#ff6600">Đăng nhập</font>
+                </th>
+            </tr>
+            <tr>
+                <td>Tên đăng nhập:</td>
+                <td><input type="text" name="txtTDNhap" size="12" /></td>
+            </tr>
+            <tr>
+                <td>Mật khẩu:</td>
+                <td><input type="password" name="pswMKhau" size="12" /></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="sbmDN" value="Đăng nhập" /></td>
+                <td><input type="reset" name="rsHB" value="Hủy bỏ" /></td>
+            </tr>
+        </table>
+    </form>
+    <?php endif; ?>
+</td>
---
+++ webbansachOnTap/index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>--oO sachhay.com Oo--</title>
+    <link rel="stylesheet" type="text/css" href="css.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+    .odd {
+        background-color: #FFBE98;
+    }
+
+    table {
+        border-collapse: collapse;
+    }
+
+    td {
+        border: 1px solid black;
+        padding: 5px;
+    }
+    </style>
+</head>
+
+<body bgcolor="#999999">
+    <center>
+        <table width="90%" border="1" cellspacing="0" bordercolor="#003300" cellpadding="0"
+            style="box-shadow: #6C0 0px 30px 150px;">
+            <!-- Header -->
+            <?php include 'header.php';
+               if (session_status() === PHP_SESSION_NONE) {
+                session_start();                
+            }
+            $isAdmin = isset($_SESSION['role']) && $_SESSION['role'] == 'admin'; 
+            $isLogin = isset($_SESSION['username']);      
+            ?>
+
+            <tr bgcolor="#92D84E">
+                <td colspan="3">
+                    <table border="0" width="100%" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td width="80"><a id="thuong" href="./">Trang chủ</a></td>
+                            <td>
+                                <font color="#FF6600">
+                                    <marquee>Cùng bạn đi tìm tri thức!</marquee>
+                                </font>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td width="200" align="left" valign="top" bgcolor="#92D84E">
+                    <table border="1" cellspacing="0" width="100%" cellpadding="0" bordercolor="#003300">
+                        <tr>
+                            <th>
+                                <font color="#FF6600">Quản lý sách</font>
+                            </th>
+                        </tr>
+                        <?php
+                        if (!$isLogin) {
+                        echo '<tr><td><a href="?page=DangKy">Đăng ký</a></td></tr>';
+                        }
+                        ?>
+
+                        <tr>
+                            <td><a href="?page=getSach">Tất cả Sách</a></td>
+                        </tr>
+                        <?php
+                        if ($isAdmin) {
+                        echo '<tr><td><a href="?page=create_book">Thêm Sách</a></td></tr>';
+                        }
+                        ?>
+                        <tr>
+                            <td><a href="?page=getGioHang">Giỏ hàng</a></td>
+                        </tr>
+                    </table>
+                </td>
+                <td valign="top" bgcolor="#FFFFFF"
+                    style="text-align: center; vertical-align: middle; background-color: white;">
+                    <?php
+                    if (isset($_GET['page'])) {
+                        $page = $_GET['page'];
+
+                        // Kiểm tra giá trị của $page
+                        switch ($page) {
+                            case 'DangKy':
+                                include 'views/DangKy.php';
+                                break;
+
+                            case 'getSach':
+                                include 'views/getSach.php';  // Gọi tệp getSach.php mà không cần thêm query string
+                                break;                                
+
+                            case 'create_book':
+                                include 'views/create_book.php';
+                                break;
+                            
+                            case 'getGioHang':
+                                include 'views/getGioHang.php';
+                                break;
+
+                            // Thêm các case khác nếu cần
+                            // Thêm các case khác nếu cần
+                            default:
+                            echo "
+                            <div class='container mt-5'>
+                                <div class='text-center'>
+                                    <h3 class='display-4 text-danger'>404 Không tìm thấy</h3>
+                                    <p class='lead text-muted'>Chào mừng, lữ khách. Bạn đã đến một trang không tồn tại, một nơi mà nội dung từng tồn tại—hoặc có thể chưa bao giờ tồn tại. Hãy dành thời gian này để dừng lại và suy ngẫm.</p>
+                                </div>
+                                <div class='card p-4'>
+                                    <p class='text-justify'>Hít một hơi thật sâu và thở ra từ từ. Lưu ý không gian xung quanh bạn, trống rỗng nhưng đầy khả năng. Hãy tưởng tượng rằng mỗi lần thở ra sẽ xóa tan sự bối rối, tạo không gian cho sự sáng suốt.</p>
+                                    <p class='text-justify'>Khi bạn ngồi với trang giấy trắng này, hãy biết rằng việc ở đây là điều bình thường. Bạn đã khám phá ra điều gì đó bất ngờ và đó là một phần của hành trình. Hãy nhẹ nhàng giải tỏa mọi sự thất vọng, biết rằng mọi con đường đều dẫn đến một nơi nào đó—kể cả con đường này.</p>
+                                    <p class='text-justify'>Bây giờ, khi bạn đã sẵn sàng, hãy từ từ quay lại tìm kiếm của mình. Tin rằng đúng trang, đúng thông tin, sẽ xuất hiện khi bạn cần. Hít một hơi thật sâu nữa và khi bạn thở ra, hãy nhấp vào nút quay lại hoặc thử lại. Internet, giống như cuộc sống, luôn đầy bất ngờ.</p>
+                                    <p class='text-justify'>Cảm ơn bạn đã dành thời gian bình tĩnh này. Hành trình của bạn vẫn tiếp tục.</p>
+                                </div>
+                            </div>
+                            ";
+                            break;                        
+                        }
+                    } else {
+                        include 'views/getSach.php'; // Trang mặc định
+                    }
+                    ?>
+                    <!-- footer -->
+                    <?php include 'footer.php'; ?>
